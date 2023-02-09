7e07fe1819dbeee55466904d79071c10c02d8339
Nikhesh <nikhesh.p@kambala.co.in>
2023-02-09 13:05:24 +0530

    table created

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './button.css';
 
 /**
- * Primary UI component for user interaction
+ * Request to be POSTed to uri :  /NorenWClientTP/QuickAuth
  */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
